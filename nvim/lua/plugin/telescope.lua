0dc83ff66243a78f301888167c9d1410af3d2d78
MikeWelsh801 <mikewelsh801@gmail.com>
2024-11-12 12:03:01 -0700

    tweakypoo and update telescope

--- nvim/lua/plugin/telescope.lua
+++ nvim/lua/plugin/telescope.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim',
     event = "CursorMoved",
-    tag = '0.1.4',
+    tag = '0.1.8',
     -- or                            , branch = '0.1.x',
     dependencies = { { 'nvim-lua/plenary.nvim' } },
     config = function()
